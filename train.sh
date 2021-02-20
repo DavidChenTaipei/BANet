a55c03b4822cdd8778831169b76c76f614a35582
David Chen <54167907+DavidChenTaipei@users.noreply.github.com>
2021-02-21 01:01:43 +0800

    Add notation

--- train.sh
+++ train.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-for (( i=1; i<=1; i=i+1 ))
+
+#python tools/banet_train.py --epoch-to-train 150 --name NAME --finetune-from ./res/NAME_OF_WEIGHTS.pth
+
+for (( i=1; i<=406; i=i+1 ))
 do
   echo 'i is '${i}
   echo '======='
-  python tools/banet_train.py --epoch-to-train 150 --name banet --finetune-from ./res/banet.pth
+  ## given the argument ''--epoch-to-train'' value '1', model will do the evaluation every epoch
+  python tools/banet_train.py --epoch-to-train 1 --name NAME --finetune-from ./res/NAME_OF_WEIGHTS.pth
 done
